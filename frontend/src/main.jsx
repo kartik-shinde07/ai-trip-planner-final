b1481da3b1d722e64d69f6d83832c4f52b7b9fc2
Rudra Admane <rudraadmane1486@gmail.com>
2026-07-02 14:37:24 +0530

    Backup before navbar redesign

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom/client";
 
 import { BrowserRouter } from "react-router-dom";
 
+import "./styles/variables.css";
+import "./styles/theme.css";
+import "./styles/animations.css";
+
 import App from "./App";
 import "./index.css";
 
